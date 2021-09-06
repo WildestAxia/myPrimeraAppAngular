e4fe1840a905feef78a529ded7cac63e07426fea
WildestAxia <61634766+WildestAxia@users.noreply.github.com>
2021-09-06 17:31:32 +0200

    commit lunes 06/06/21

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -9,17 +9,44 @@ export class EmpleadoComponent implements OnInit {
 
 
   nombre = "David";
+
   apellido = "Martinez";
+
   edad = 18;
-  empresa = "Tu madre Calva";
+
+  //empresa = "Tu madre Calva";
+
+  habilitacionCuadro = false;
+
+  usuRegistrado = false;
+
+  textoDeRegistro = "No hay nadie registrado" ;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+
+
   /*public getEdad(){
     return this.edad;
   }
   */
+  getRegistroUsuario(){
+    this.usuRegistrado = true;
+  }
+
+  setUsuarioRegistrado(event: Event){
+
+    //alert(Event.target);
+    if((<HTMLInputElement>event.target).value=="si"){
+        this.textoDeRegistro="El usuario se acaba de registrar"
+    }else{
+      this.textoDeRegistro="No hay nadie registrado"
+    }
+  }
+
 
 }
